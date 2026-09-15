ddcfe278b75ad822f735e6d0f67ac8152b93d7de
Rsantos081 <novo-ruan.santostech@gmail.com>
2026-09-15 11:33:09 -0300

    Adicionando Endpoints de Inventario ao Projeto

--- app/src/routes.py
+++ app/src/routes.py
@@ -72,3 +72,88 @@ def get():
         }
         marcas_list.append(marcas_data)
     return jsonify (marcas_list)
+
+@inventario_bp.route('/add', methods = ["POST"])
+@login_required
+def add_modelo():
+    data = request.json
+    if 'modelo' in data and 'transmisao' in data and 'motor' in data and 'combustivel' in data and 'marcas_id' in data:
+        marca = Marcas.query.get(data['marcas_id'])
+        if not marca:
+            return jsonify ({"mensagem":"Marca não Encontrada"}), 404
+        inventarios = Inventario(
+                modelo=data['modelo'], 
+                transmisao=data['transmisao'], 
+                motor=data['motor'], 
+                combustivel=data['combustivel'],
+                marcas_id =data['marcas_id']
+        )   
+        db.session.add(inventarios)
+        db.session.commit()
+        return jsonify ({"mensagem":"Modelo de Carro Adicionado com Sucesso"}), 201
+    return jsonify ({"mensagem":"Crendencias do Modelo Invalido"}), 400
+
+@inventario_bp.route('/delete/<int:inventario_id>', methods = ['DELETE'])
+@login_required
+def delete_modelo(inventario_id):
+    inventarios = Inventario.query.get(inventario_id)
+    if inventarios:
+        db.session.delete(inventarios)
+        db.session.commit()
+        return jsonify ({"mensagem":"Modelo de carro deletado com sucesso"}), 200
+    return jsonify ({"mensagem":"Modelo de carro nao Encontrado"}), 400
+
+@inventario_bp.route('/<int:inventario_id>', methods = ["GET"])
+@login_required
+def get_modelo(inventario_id):
+    inventarios = Inventario.query.get(inventario_id)
+    if inventarios:
+        return jsonify ({
+            'id': inventarios.id,
+            'modelo':inventarios.modelo,
+            'transmisao':inventarios.transmisao,
+            'motor':inventarios.motor,
+            'combustivel':inventarios.combustivel,
+            'marcas_id':inventarios.marcas_id
+        })
+    return jsonify ({"mensagem":"Modelo de carro nao encontrado"}), 404
+
+@inventario_bp.route('/<int:inventario_id>', methods = ["PUT"])
+@login_required
+def mod_modelo(inventario_id):
+    inventarios = Inventario.query.get(inventario_id)
+    if not inventarios:
+        return jsonify ({"mensagem":"Produto não Encontrado"}), 404
+    data = request.json
+    if 'modelo' in data:
+        inventarios.modelo = data['modelo']
+        
+    if 'transmissao' in data:
+        inventarios.transmissao = data['transmissao']
+    
+    if 'motor' in data:
+        inventarios.motor = data['motor']
+    
+    if 'combustivel' in data:
+        inventarios.combustivel = data['combustivel']
+
+    db.session.commit()
+    return jsonify ({"mensagem":"Produto Atualizado com Sucesso"}), 200         
+
+@inventario_bp.route('/', methods = ['GET'])
+@login_required
+def get():
+    inventario = Inventario.query.all()
+    inventario_list = []
+    for inventarios in inventario:
+        inventario_date ={
+            'id':inventarios.id,
+            'modelo':inventarios.modelo,
+            'transmissao':inventarios.transmissao,
+            'motor':inventarios.motor,
+            'combustivel':inventarios.combustivel,
+            'marcas_id':inventarios.marcas_id
+        }
+        inventario_list.append(inventario_date)
+    return jsonify (inventario_list)
+ 
