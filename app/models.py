3b52bf78d223cdfa3556107bf1e81f4e966a3859
Rsantos081 <novo-ruan.santostech@gmail.com>
2026-09-23 13:52:05 -0300

    Alteraçao nas tabelas, e trazendo relacionamento entre elas

--- app/models.py
+++ app/models.py
@@ -13,14 +13,16 @@ class Inventario(db.Model):
     motor = db.Column(db.String(255), nullable=False)
     combustivel = db.Column(db.String(255), nullable=False)
     marcas = db.relationship('Marcas', backref='Inventario', lazy = True)
-    marcas_id = db.Column(db.Integer, db.ForeignKey('marcas.id'))
+    marcas_id = db.Column(db.Integer, db.ForeignKey('marcas.id'), nullable = False)
 
+   
 class Clientes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome_cliente = db.Column(db.String(255), nullable=False)
     sobrenome_cliente = db.Column(db.String(255), nullable=False)
     cpf = db.Column(db.String(20),nullable=False, unique = True)
     endereco = db.Column(db.String(255), nullable=False)
+    
     
 class Pagamentos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,14 +33,14 @@ class Pagamentos(db.Model):
     inventario_id = db.Column(db.Integer, db.ForeignKey('inventario.id'))
     clientes_id = db.Column(db.Integer, db.ForeignKey('clientes.id'))
     
-class Lacacao(db.model):
+class Locacoes(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     data_inicio = db.Column(db.Date, nullable=False)
     data_prevista_devolucao = db.Column(db.Date, nullable=False)
-    status_carro = db.Column(db.String(100), nullable=False)
-    valor = db.Column(db.Integer, nullable=False)
-    inventario = db.relationship('Inventario', backref='Locacao', lazy= True)
-    cliente = db.relationship('Clientes', backref='Locacao', lazy= True)
-    inventario_id = db.Column(db.Integer, db.ForeignKey('inventario.id'))
-    clientes_id = db.Column(db.Integer, db.ForeignKey('clientes.id'))
+    status_locacao = db.Column(db.String(100), nullable=False)
+    valor = db.Column(db.Numeric(10,2), nullable=False)
+    inventario = db.relationship('Inventario', backref='Locacoes', lazy= True)
+    cliente = db.relationship('Clientes', backref='Locacoes', lazy= True)
+    inventario_id = db.Column(db.Integer, db.ForeignKey('inventario.id'), nullable = False)
+    clientes_id = db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable = False)
     
